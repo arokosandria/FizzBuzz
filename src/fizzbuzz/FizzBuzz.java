965734f7c798c8106275e03c63b8de0e87d16a57
arokosandria <kmsa@interia.pl>
2019-03-02 21:30:12 +0100

    fizzbuzz

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,31 +2,34 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
-    public String FizzConvert(int number){
-        if (number==0){
+    public String FizzConvert(int number) {
+        if (number == 0) {
             return "";
         }
-        if(number%15==0){
+        if (number % 15 == 0) {
             return "FizzBuzz";
         }
-        if(number%3==0){
+        if (number % 3 == 0) {
             return "Fizz";
         }
-        if(number%5==0){
+        if (number % 5 == 0) {
             return "Buzz";
         }
 
         return String.valueOf((number));
-}
-        public void print(){
-            for (int i=1; i<=100; i++){
-                System.out.println(FizzConvert(i));
+    }
 
-        }}
-    public static void main(String[] args) {
-        FizzBuzz f =new FizzBuzz();
-        f.print();
+    public void print() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(FizzConvert(i));
 
         }
     }
+
+    public static void main(String[] args) {
+        FizzBuzz f = new FizzBuzz();
+        f.print();
+
+    }
+}
 
--- src/fizzbuzz/FizzBuzzTest.java
+++ src/fizzbuzz/FizzBuzzTest.java
@@ -6,39 +6,43 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
 
-@Test
+    @Test
 
-public void testFizzBuzzZero() {
-    FizzBuzz f=new FizzBuzz();
-    assertEquals("", f.FizzConvert(0));
+    public void testFizzBuzzZero() {
+        FizzBuzz f = new FizzBuzz();
+        assertEquals("", f.FizzConvert(0));
+    }
+
+    @Test
+
+    public void testFizzBuzzThree() {
+        FizzBuzz f = new FizzBuzz();
+        assertEquals("Fizz", f.FizzConvert(3));
+
+    }
+
+    @Test
+    public void testFizzBuzzFive() {
+        FizzBuzz f = new FizzBuzz();
+        assertEquals("Buzz", f.FizzConvert(5));
+
+    }
+
+    @Test
+    public void testFizzBuzzFiveteen() {
+        FizzBuzz f = new FizzBuzz();
+        assertEquals("FizzBuzz", f.FizzConvert(15));
+
+    }
+
+    @Test
+    public void testFizzBuzzAnything() {
+        FizzBuzz f = new FizzBuzz();
+        assertEquals("2", f.FizzConvert(2));
+
+    }
+
+
 }
-    @Test
-
-    public void testFizzBuzzThree(){
-        FizzBuzz f=new FizzBuzz();
-        assertEquals("Fizz",f.FizzConvert(3));
-
-    }
-    @Test
-    public void testFizzBuzzFive(){
-        FizzBuzz f=new FizzBuzz();
-        assertEquals("Buzz",f.FizzConvert(5));
-
-    }
-    @Test
-    public void testFizzBuzzFiveteen(){
-        FizzBuzz f=new FizzBuzz();
-        assertEquals("FizzBuzz",f.FizzConvert(15));
-
-    }
-    @Test
-    public void testFizzBuzzAnything(){
-        FizzBuzz f=new FizzBuzz();
-        assertEquals("2",f.FizzConvert(2));
-
-    }
-
-
-    }
 
 
