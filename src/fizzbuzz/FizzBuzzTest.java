1fee1d0e16f8d7c7bc696447313df4cb034ed05e
arokosandria <kmsa@interia.pl>
2019-03-02 21:02:50 +0100

    test

--- src/fizzbuzz/FizzBuzzTest.java
+++ src/fizzbuzz/FizzBuzzTest.java
@@ -6,31 +6,34 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
 
-
-    FizzBuzz f=new FizzBuzz();
 @Test
 
 public void testFizzBuzzZero() {
+    FizzBuzz f=new FizzBuzz();
     assertEquals("", f.FizzConvert(0));
 }
     @Test
 
     public void testFizzBuzzThree(){
+        FizzBuzz f=new FizzBuzz();
         assertEquals("Fizz",f.FizzConvert(3));
 
     }
     @Test
     public void testFizzBuzzFive(){
+        FizzBuzz f=new FizzBuzz();
         assertEquals("Buzz",f.FizzConvert(5));
 
     }
     @Test
     public void testFizzBuzzFiveteen(){
+        FizzBuzz f=new FizzBuzz();
         assertEquals("FizzBuzz",f.FizzConvert(15));
 
     }
     @Test
     public void testFizzBuzzAnything(){
+        FizzBuzz f=new FizzBuzz();
         assertEquals("2",f.FizzConvert(2));
 
     }
