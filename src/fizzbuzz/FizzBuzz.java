edde692528a582da0de27b6a62da6ab3db4b9490
arokosandria <kmsa@interia.pl>
2019-03-02 21:59:01 +0100

    fizzbuzz

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -26,10 +26,6 @@ public class FizzBuzz {
         }
     }
 
-    public static void main(String[] args) {
-        FizzBuzz f = new FizzBuzz();
-        f.print();
 
-    }
 }
 
---
+++ src/fizzbuzz/FizzBuzzMain.java
@@ -0,0 +1,8 @@
+package fizzbuzz;
+
+public class FizzBuzzMain {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        fizzBuzz.print();
+    }
+}
